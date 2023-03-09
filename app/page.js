1b802856087900c888ff589cec37cef21e5cb9df
Agnes Pilar <agnes.pilar@yahoo.de>
2023-03-09 18:55:14 +0100

    Try styling

--- app/page.js
+++ app/page.js
@@ -9,12 +9,25 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="text-center flex justify-center">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <h1 className="text-xl text-red-600">BE BOULDER</h1>
-        <Link href="/locations">
-          Discover the best bouldering locations in Austria!
-        </Link>
+    <main>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("/images/hero.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60" />
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">BE BOULDER</h1>
+            <p className="mb-5">
+              Discover the best bouldering locations in Austria!
+            </p>
+            <button className="btn btn-primary">
+              <a href="/locations">Get Started</a>
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
