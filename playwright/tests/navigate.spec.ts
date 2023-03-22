a47d1b1b01283a4ece6a87de895de84f65d0d4e0
Agnes Pilar <agnes.pilar@yahoo.de>
2023-03-22 18:32:14 +0100

    Write first playwright test

--- playwright/tests/navigate.spec.ts
+++ playwright/tests/navigate.spec.ts
@@ -1,1 +1,13 @@
 import { expect, test } from '@playwright/test';
+
+test('navigation test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await expect(page.getByRole('heading', { name: 'BE BOULDER' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Get started' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  await expect(page).toHaveURL('http://localhost:3000/locations');
+});
