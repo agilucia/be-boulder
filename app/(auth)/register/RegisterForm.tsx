f10afbf2f2070889025685bc8f62185120848b40
Agnes Pilar <agnes.pilar@yahoo.de>
2023-03-25 19:38:29 +0100

    Fix card sizes for login and register

--- app/(auth)/register/RegisterForm.tsx
+++ app/(auth)/register/RegisterForm.tsx
@@ -12,7 +12,7 @@ export default function RegisterForm(props: { returnTo?: string | string[] }) {
 
   return (
     <form
-      className="card w-96 bg-base-100 shadow-xl bg-opacity-90"
+      className="card w-80 bg-base-100 shadow-xl bg-opacity-90"
       onSubmit={async (event) => {
         event.preventDefault();
 
